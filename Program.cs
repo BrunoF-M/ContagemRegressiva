0e1923c369ae1d4155acadd24e7a852ba6887e15
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-04 22:22:10 -0300

    resolução ContagemRegressiva

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá,Soldado Você esta pronto? (presione qualquer tecla)");
+            Console.ReadKey();
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.WriteLine("**FOGO!!!**");
+        }
+    }
+}
